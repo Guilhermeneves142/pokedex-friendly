73f50ff7590bdd8236b83d84ed306144d25aa1fe
Guilhermeneves142 <guilherme_n2010@hotmail.com>
2022-10-11 00:06:24 -0300

    filtro finalizado

--- src/views/PokedexView.vue
+++ src/views/PokedexView.vue
@@ -3,7 +3,7 @@
     <div>
       <h1>Pokédex</h1>
       <div class="search-area">
-        <div class="flex items-center">
+        <div class="flex items-center bg-[#323232]">
           <div class="w-1/2">
             <h2>Nome ou número</h2>
             <div id="search">
@@ -22,9 +22,8 @@
             </p>
           </div>
         </div>
-
         <div id="filter">
-          <div class="grid grid-cols-12 gap-4">
+          <div class="grid grid-cols-12 gap-4" v-if="openFilter">
             <div class="self-baseline col-span-3">
               <h2>Tipo e Fraqueza</h2>
             </div>
@@ -34,6 +33,22 @@
 
             <div class="col-span-5 row-span-2">
               <h2 class="self-baseline">Habilidade</h2>
+              <div class="mt-2 group">
+                <div
+                  class="w-3/4 bg-[#313131] h-10 rounded-md flex items-center"
+                >
+                  <img
+                    src="/pokeball.ico"
+                    class="mx-4 bg-white rounded-full w-7 h-7"
+                  />
+                  <span class="grow"> Todas </span>
+                  <div
+                    class="h-full w-10 flex items-center justify-center group-hover:bg-[#282828] rounded-r-md"
+                  >
+                    <span class="mdi mdi-chevron-down text-2xl" />
+                  </div>
+                </div>
+              </div>
             </div>
 
             <div class="col-span-3 row-span-6">
@@ -57,29 +72,56 @@
             <div class="col-span-5 row-span-2">
               <h2 class="self-baseline">Altura</h2>
               <div class="flex mt-3">
-                <div class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl">A</div>
-
-                <div class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl mx-6">
-                  A
+                <div
+                  class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl flex justify-center items-center"
+                >
+                  <img
+                    src="/bullbasaur.png"
+                    class="w-10 h-10"
+                    title="Pequeno"
+                  />
                 </div>
 
-                <div class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl">A</div>
+                <div
+                  class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl mx-6 flex justify-center items-center"
+                >
+                  <img src="/squirtle.png" class="w-12 h-12" title="Médio" />
+                </div>
+
+                <div
+                  class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl flex justify-center items-center"
+                >
+                  <img src="/charmander.png" class="w-16 h-16" title="Grande" />
+                </div>
               </div>
             </div>
             <div class="col-span-5 row-span-3">
               <h2 class="self-baseline">Peso</h2>
               <div class="flex mt-3">
-                <div class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl">A</div>
-
-                <div class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl mx-6">
-                  A
+                <div
+                  class="bg-gray-50 w-[5.75em] h-[4.75em] group rounded-xl flex justify-center items-center"
+                >
+                  <img src="/bellsprout.png" class="w-16 h-16" title="leve" />
                 </div>
 
-                <div class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl">A</div>
+                <div
+                  class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl mx-6 flex justify-center items-center"
+                >
+                  <img src="/eevee.png" class="w-16 h-16" title="médio" />
+                </div>
+
+                <div
+                  class="bg-gray-50 w-[5.75em] h-[4.75em] rounded-xl flex justify-center items-center"
+                >
+                  <img src="/snorlax.png" class="w-16 h-16" title="pesado" />
+                </div>
               </div>
             </div>
-            <div class="col-span-7">
-              <h2 class="baseline">Intervalos de números</h2>
+            <div class="col-span-7" id="range">
+              <h2 class="baseline mr-4">Intervalos de números</h2>
+              <input class="input-range" />
+              <span class="mx-4"> - </span>
+              <input class="input-range" />
             </div>
             <div class="col-span-5">
               <button class="bg-main btn-option">Redefinir</button>
@@ -89,6 +131,22 @@
               </button>
             </div>
           </div>
+          <div
+            :class="['search-open', { 'mt-6': openFilter }]"
+            @click="openFilter = !openFilter"
+          >
+            <span>{{
+              openFilter ? "Esconder busca avançada" : "Mostrar busca avançada"
+            }}</span>
+            <span
+              :class="[
+                'mdi ml-2',
+                openFilter
+                  ? 'mdi-chevron-up-circle'
+                  : 'mdi-chevron-down-circle',
+              ]"
+            ></span>
+          </div>
         </div>
       </div>
     </div>
@@ -97,9 +155,11 @@
 
 <script setup lang="ts">
 import { useTypesFilter } from "@/stores/TypesFilter";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
 const types = useTypesFilter();
+
+const openFilter = ref(false);
 
 const typesOne = computed(() => {
   return types.types.filter((e, index) => index < 9);
@@ -131,6 +191,9 @@ const typesTwo = computed(() => {
 #filter {
   @apply w-full bg-[#616161];
 }
+#range {
+  @apply flex items-center first:mr-2 ml-0;
+}
 h1 {
   @apply text-main text-4xl font-normal py-4 px-3 left-0;
 }
@@ -141,10 +204,10 @@ h2 {
   @apply font-medium text-xl text-white px-6 py-3 rounded;
 }
 .search-area {
-  @apply w-full  absolute left-0 bg-dark text-gray-50;
+  @apply w-full  absolute left-0  text-gray-50;
 }
 .search-area > div {
-  @apply px-[13%] py-8;
+  @apply px-[13%] py-4;
 }
 .type-info:first-child {
   @apply w-24 p-1 border-2 border-main rounded text-center cursor-pointer;
@@ -156,5 +219,11 @@ h2 {
 .type-info:nth-child(2)[selected="true"],
 .type-info:nth-child(3)[selected="true"] {
   @apply bg-[#30a7d7];
+}
+.input-range {
+  @apply rounded-md w-20 p-2 text-gray-900;
+}
+.search-open {
+  @apply bg-[#616161] w-fit px-20 mx-auto p-0 flex text-lg  cursor-pointer !important;
 }
 </style>
